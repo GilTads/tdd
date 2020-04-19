afc45a5442915de053e798cc421f89eaccab3557
GilTads <gil.tads@gmail.com>
2020-04-19 15:22:28 -0400

    removed test

--- test/server.test.js
+++
@@ -1,9 +0,0 @@
-import supertest from 'supertest'
-// import server from '../src/server'
-
-const request = supertest('http://localhost:3000')
-
-test('Deve responder na porta 3000', async () => {
-  const data = await request.get('/')
-  await expect(data.status).toBe(200)
-})
