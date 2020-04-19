9c8c3178a5c5ca15fcb99f35a268b51ea8908f7a
GilTads <gil.tads@gmail.com>
2020-04-19 11:41:19 -0400

    make server test

--- test/server.test.js
+++ test/server.test.js
@@ -1,10 +1,9 @@
 import supertest from 'supertest'
+// import server from '../src/server'
 
 const request = supertest('http://localhost:3000')
 
 test('Deve responder na porta 3000', async () => {
-  // Acessar a url http://localhost:3000
   const data = await request.get('/')
-  expect(data.status).toBe(400)
-  // Devo verificar se a resposta foi 200
+  await expect(data.status).toBe(200)
 })
