7ad1184da2ac1c15fe59a84ceda9ad1a33d63401
GilTads <gil.tads@gmail.com>
2020-04-19 11:54:38 -0400

    refactoring server tests

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
 import app from './app'
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
-
 app.listen(3000)
---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+import supertest from 'supertest'
+import app from '../src/app'
+
+const request = supertest(app)
+
+test('Deve responder na rota raiz', async () => {
+  const data = await request.get('/')
+  await expect(data.status).toBe(200)
+})
