62d9fd8bf2f20ff348c776436e7eb0266182568c
GilTads <gil.tads@gmail.com>
2020-04-21 20:48:53 -0400

    create some user tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,17 +1,17 @@
 import supertest from 'supertest'
-import app from '../src/app'
+import Server from '../server'
 
-const request = supertest(app)
+const request = supertest(Server)
 
 test('Deve listar todos os usuários', async () => {
-  const res = await request.get('/users')
+  const res = await request.get('/user')
   await expect(res.status).toBe(200)
   await expect(res.body).toHaveLength(1)
   await expect(res.body[0]).toHaveProperty('name', 'Gil')
 })
 
 test('Deve inserir um usuário com sucesso', async () => {
-  const res = await request.post('/users').send({ name: 'Lu', email: 'lu@email.com' })
+  const res = await request.post('/user').send({ name: 'Lu', email: 'lu@email.com' })
   await expect(res.status).toBe(201)
   await expect(res.body.name).toBe('Lu')
 })
